dfdcd55b2cada644d40a419f6b9bce0dad76ee11
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-07-13 18:48:26 +0300

    feat: add basename

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='rick-morty-React-TS-ApolloClient-/'>
       <ApolloProvider client={client} >
         <App />
       </ApolloProvider>
